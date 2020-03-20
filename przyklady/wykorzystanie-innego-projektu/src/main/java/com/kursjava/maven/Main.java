cbd7b966f41d3a71cfb11cac568a711c3dd12fbf
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2020-03-20 18:46:23 +0100

    Rozdzial o zaleznosciach - scope, lokalne projekty

---
+++ przyklady/wykorzystanie-innego-projektu/src/main/java/com/kursjava/maven/Main.java
@@ -0,0 +1,9 @@
+package com.kursjava.maven;
+
+import com.kursjava.maven.FactorialCounter;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Silnia 10 = " + FactorialCounter.factorial(10));
+  }
+}
