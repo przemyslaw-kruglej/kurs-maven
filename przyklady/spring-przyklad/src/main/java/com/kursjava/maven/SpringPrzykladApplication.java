802b2c8a08f1968f1cd280bea8fcdc04df202cd8
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2020-03-21 12:37:37 +0100

    Rozdzial o zaleznosciach - ostatni podrozdzial

---
+++ przyklady/spring-przyklad/src/main/java/com/kursjava/maven/SpringPrzykladApplication.java
@@ -0,0 +1,13 @@
+package com.kursjava.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrzykladApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrzykladApplication.class, args);
+	}
+
+}
---
+++ przyklady/spring-przyklad/src/test/java/com/kursjava/maven/SpringPrzykladApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursjava.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrzykladApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
